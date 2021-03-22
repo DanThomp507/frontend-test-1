c36078bfd5e8267d33504b59f3462cacabebad42
Daniel Thompson <DanThomp507@gmail.com>
2021-03-22 09:03:00 -0400

    fix: abstracted truncation to utility folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-
 import "./App.css";
-
 import Autocomplete from "./components/Autocomplete";
 import ProductDetail from "./components/ProductDetail";
 
 const App = () => {
-  const [productId, setProductId] = useState("");
+  const [productId, setProductId] = useState();
 
   const getProductId = id => {
     setProductId(id);
